21a7eff52a87802db1c8e252466e451dcc5938d7
anna <annasmolkina1@gmail.com>
2021-06-15 22:42:51 +0300

    main

---
+++ ishop/dist/ishop.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var ishop = React.createClass({
+  displayName: "ishop",
+  propTypes: {
+    nameShop: React.PropTypes.string.isRequired,
+    products: React.PropTypes.array.isRequired
+    /*
+    products: React.PropTypes.arrayOf (
+        React.PropTypes.shape ({
+            nameProduct: React.PropTypes.string.isRequired,
+            code: React.PropTypes.number.isRequired,
+            price:React.PropTypes.number.isRequired,
+            url: React.PropTypes.string.isRequired,
+            free: React.PropTypes.number,
+            text: React.PropTypes.string.isRequired,
+        })
+    )
+    */
+
+  },
+  render: function render() {
+    var productList = this.props.products.map(function (v) {
+      return React.DOM.div({
+        key: v.code,
+        className: 'ishop__Product'
+      }, React.DOM.span(v.url), React.DOM.span(v.text, v.nameProduct), React.DOM.span(v.text, v.price), React.DOM.span(v.text, v.free));
+    });
+    return React.DOM.div({
+      className: 'ishop'
+    }, React.DOM.div({
+      className: 'ishop__Name'
+    }, this.props.nameShop), React.DOM.div({
+      className: 'ishop__Products'
+    }, productList));
+  }
+});
+//# sourceMappingURL=ishop.dev.js.map
