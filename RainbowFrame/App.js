1324aa1490a5d4bce31ca1a46edf1fefde4ea6b9
AnnaSmolkina <annasmolkina1@gmail.com>
2021-07-05 16:30:09 +0300

    main

---
+++ RainbowFrame/App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+// если необходимо, вид сборки можно проверить в коде:
+// if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV !== 'production') {
+
+ReactDOM.render( 
+  <RainbowFrame />
+, document.getElementById('container') );
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+
+  render() {
+    let colors=['yellow', 'blue', 'red', 'green', 'grey', 'purple', 'orange'];
+    let code=this.props.children;
+        this.props.colors.forEach (color => 
+            <div style={{border:"solid 1px "+this.props.color,padding:"10px"}} >
+                Hello! My name is Anna.
+            </div>
+         );
+         return code;
+        
+    };
+}
+export default RainbowFrame;
+   
