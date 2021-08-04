d7ce65a9d4710ed6a0edec2886d3822768f4fc86
AnnaSmolkina <annasmolkina1@gmail.com>
2021-08-04 19:53:27 +0300

    main

---
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+function withRainbowFrame (colors) {
+  return function (Component) {
+    return props => (
+      <div style={{border:"solid 1px "+colors,padding:"10px"}}>
+        <Component {...props} />
+      </div>
+    );
+  };
+}
+
+export { withRainbowFrame};
